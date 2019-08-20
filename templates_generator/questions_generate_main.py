df97fb4c7a6299851bb8d7b7acab48b8861df00b
StuartCHEN <chen.jiazheng.stuart@foxmail.com>
2019-08-20 15:28:20 +0800

    update

--- templates_generator/questions_generate_main.py
+++ templates_generator/questions_generate_main.py
@@ -2,41 +2,35 @@
 """
  
 """
-import os
+
 import pandas as pd
-import vec_utils
 from vec_utils import sentence_encoder
 import wiki_extract
 import ntriples_extract
-import templates_allocator
-import semantic_parser
 from semantic_parser import get_all_instances
-
+import argparse
+parser = argparse.ArgumentParser(description='manual to this script')
+parser.add_argument('--dbo_class', type=str, default = None)
+args = parser.parse_args()
 
 
 
 
 if __name__ == "__main__":
     
-    data = pd.read_csv("../data/QALD-7.csv")    
-    rows = [str(list(row[-1])[0]) for row in data.iterrows()]
-    rows = [str(row).strip(';').strip() for row in rows ]
-    classes = list(set([str(row).split(';')[0] for row in rows if row.startswith('dbo:')] )) 
-    classes = [str(clas).replace('dbo:','').strip() for clas in classes ];
-    
-    for clas in classes:
-        instances = get_all_instances(clas)
-        if (instances is not None) and ( len(instances)>=1 ):
-            for instance in instances:
-                try:
-                   savepath = wiki_extract.extract_page(instance, clas)   
-                except:
-                    print("\nThe Wiki page for entity of %(instance)s in the class %(class)s does not exist"%{"instance":instance, "class":clas }) 
-                    pass;
-                try:
-                    ntriples_extract.downloadFile(instance, savepath)
-                except:
-                    print("\nThe ntriples file for entity of %(instance)s in the class %(class)s does not exist"%{"instance":instance, "class":clas })   
-                    pass;
+    dbo_class = args.dbo_class 
+    instances = get_all_instances(dbo_class)
+    if (instances is not None) and ( len(instances)>=1 ):
+        for instance in instances:
+            try:
+               savepath = wiki_extract.extract_page(instance, dbo_class)   
+            except:
+                print("\nThe Wiki page for entity of %(instance)s in the class %(class)s does not exist"%{"instance":instance, "class":dbo_class }) 
+                pass;
+            try:
+                ntriples_extract.downloadFile(instance, savepath)
+            except:
+                print("\nThe ntriples file for entity of %(instance)s in the class %(class)s does not exist"%{"instance":instance, "class":dbo_class })   
+                pass;
     
     
